fde6c8251070cd09b15b0f906ec1ebac76b4f5b8
DavideDaniel <davidedaniel@gmail.com>
2016-03-07 23:06:46 -0800

    Lesson 2

--- week_1/main.js
+++ week_1/main.js
@@ -1,10 +1,21 @@
-var data = [ 100, 200, 150, 300, 400, 600 ];
+var dataSet = [10, 20, 15, 30, 40, 55, 30, 50, 60];
 
-  d3.select('body').selectAll('div')
-    .data(data)
-    .enter() // starting d3
-    .append('div') // creating a div
-    .attr('class', 'bar') // assigning class
-    .style('height', function (data) {
-      return data + 'px'; // returning data with px appended
-    });
+var svg = d3.select('#barChart').append('svg')
+  .attr('width', 600)
+  .attr('height', 250);
+
+svg.selectAll('rect') // using svg variable reference
+  .data(dataSet)
+  .enter() // starting d3
+  .append('rect') // creating a rect this time
+  .attr('class', 'bar') // assigning class
+  .attr('x', function(data, index) {
+    return index * 20
+  })
+  .attr('y', function(data) {
+    return 250 - data
+  })
+  .attr('width', 15)
+  .style('height', function(data) {
+    return data // returning data for height value; Note no px is needed with svg
+  })
