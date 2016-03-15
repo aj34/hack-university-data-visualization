ed7beb38fceb8c8e49b053816e83ace259c9ae16
DavideDaniel <davidedaniel@gmail.com>
2016-03-14 21:01:34 -0700

    Scatter plots, circles, g, transform/translate

--- week_1/main.js
+++ week_1/main.js
@@ -1,62 +1,43 @@
-var legislator = {
-  name: 'Ron Wyden',
-  sectors: [{
-    money_from_pacs: 589360,
-    money_from_indivs: 1109475,
-    sector_name: 'Finance/Insur/RealEst'
-  }, {
-    money_from_pacs: 782240,
-    money_from_indivs: 479476,
-    sector_name: 'Health'
-  }, {
-    money_from_pacs: 184676,
-    money_from_indivs: 638566,
-    sector_name: 'Lawyers & Lobbyists'
-  }, {
-    money_from_pacs: 104350,
-    money_from_indivs: 71799,
-    sector_name: 'Transportation'
-  }]
-}
-var dataSet = legislator.sectors.map(function(item) {
-    return item.money_from_pacs
-  }) // use map to get a new dataset
+var dataset = _.map(_.range(25), function (i) {
+  return {
+    x: Math.random() * 100,
+    y: Math.random() * 100,
+    radius: Math.random() * 20
+  };
+}); // using underscore for random data for now
 
-var height = 250; // set vars for height & width
-var width = 600;
+var margin = {top: 0, right: 0, bottom: 0, left: 0};
 
-var yScale = d3.scale.linear()
-  .domain([0, d3.max(dataSet)*1.1]) // domain now with d3.max
-  .range([0, height]) // set yScale linear
-var xScale = d3.scale.ordinal() // orders
-  .domain(dataSet)
-  .rangeBands([0, width], 0.25, 0.25); // (width of data), padding between, padding outside
+var width = 600 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
 
-  var colorScale = d3.scale.linear() // linear - min / max
-  //.domain([0,d3.max(dataSet)]) // for data based
-  .domain([0,dataSet.length]) // for position based
-  .range(['tomato','cornflowerBlue'])
+var svg = d3.select('#scatterChart').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')'); // transform the x,y value with translate
 
-var svg = d3.select('#barChart').append('svg')
-  .attr('width', width)
-  .attr('height', height)
+  var yScale = d3.scale.linear()
+    .domain([0, d3.max(dataset, function (data) {
+    return data.y;
+  })])
+   .range([height, 0]);
 
-svg.selectAll('rect')
-  .data(dataSet)
+var xScale = d3.scale.linear()
+  .domain([0, 100])
+  .range([0, width]);
+
+svg.selectAll('circle')
+  .data(dataset)
   .enter()
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', function(data, index) {
-    return xScale(data) // using xScale on data
-  })
-  .attr('y', function(data) {
-    return height - yScale(data) // using yScale on data
-  })
-  .attr('width', xScale.rangeBand) // width determined by xScale.rangeBand
-  .style('height', function(data) {
-    return yScale(data) // height determined by yScale
-  })
-  .attr('fill', function(data,i) {
-   // return colorScale(data) // for data based
-    return colorScale(i) // for position based
-  })
+  .append('circle') // creating circles
+  .attr('class', 'bubble')
+  .attr('cx', function (data) { // determining the center point x value
+  return xScale(data.x);
+})
+  .attr('cy', function (data) { // determining the center point y value
+  return yScale(data.y);
+})
+  .attr('r', function (data) { // determining the radius of the circle
+  return data.radius;
+});
