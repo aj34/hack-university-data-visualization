0700e1e44aa660c9f3780e0d05315f52e4270b9f
DavideDaniel <davidedaniel@gmail.com>
2016-04-05 00:50:24 -0700

    discussion in slack

--- lessons/README.md
+++ lessons/README.md
@@ -1,27 +1,2 @@
-# Lesson 10
----
+## Completed Project 1 Example
 
-## Sunday continuation of Project 1
-
-### Topics covered in class on Sunday
-
-##### d3 & js related
-```js
-
-// function to help us sort through the dates
-function sortByDates(a, b) {
-  return a.date - b.date; // dates convert to number so subtraction gives us the sort method
-}
-
-// grouping data and working with it
-d3.nest() // allows us to group data
-    .key( function(d) {return d./* key */}) // the key you want to group by
-    .sortKeys(d3.ascending) // can sort here as well
-    .rollup(function(values) { // rollup allows us to run function on each "leaf" element
-      return d3.sum(values,  function(d) { // values are the leaf elements here
-        return d./* some value you want to work with */;
-      })
-    })
-    .entries(/* array */); // this applies the nest operator to the array of data
-
-```
--- lessons/main.js
+++ lessons/main.js
@@ -5,77 +5,113 @@ var margin = {
   left: 70
 }
 
-var width = 800 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+var width = 1200 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
 
-var filerId = 13920;
+var parseDate = d3.time.format('%Y-%m-%d').parse;
 
-var url = 'http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/' + filerId + '/';
+var line = d3.svg.line()
+  .x(function(d) {
+    return x(d.date);
+  })
+  .y(function(d) {
+    return y(d.amount);
+  })
+  .interpolate('linear');
 
-d3.json(url, function(json) {
-  var data = json;
-  var parseDate = d3.time.format('%Y-%m-%d').parse;
-  var dataSet = data.map(function(item) {
+var svg = d3
+  .select('#content')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height-2, 0]);
+
+var xAxis = d3.svg.axis().scale(x)
+  .orient('bottom').ticks(12);
+var yAxis = d3.svg.axis().scale(y)
+  .orient('left').ticks(10);
+
+function fetchData(id) {
+  var url = 'http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/' + id + '/';
+  var fetchedData = new Promise(function(resolve, reject) {
+    $.getJSON(url, function(json) {
+      resolve(json);
+    })
+  })
+  return fetchedData;
+}
+
+function sortByDates(a, b) {
+  return a.date - b.date;
+}
+
+function formatData(data) {
+  var dataSet = data.map((item) => {
     return {
       date: parseDate(item.tran_date),
       amount: item.amount
     }
-  })
-
-  // function to help us sort through the dates
-  function sortByDates(a, b) {
-    return a.date - b.date; // dates convert to number so subtraction gives us the sort method
-  }
-
-  var dates = _.map(dataSet, 'date');
-  var amounts = _.map(dataSet, 'amount');
-
-  // defining the x and y values
-  var x = d3.time.scale() // determined through d3.time.scale function
-    .domain(d3.extent(dates)) // using the extent method on dates array
-    .range([0, width]);
-  var y = d3.scale.linear()
-    .domain(d3.extent(amounts))
-    .range([height, 0]);
-
-  // sort data here
+  });
   dataSet.sort(sortByDates);
+  return dataSet;
+}
 
-  // defining axes
-  var xAxis = d3.svg.axis().scale(x)
-    .orient('bottom').ticks(6);
-  var yAxis = d3.svg.axis().scale(y)
-    .orient('left').ticks(10);
+function visualize(data) {
+  console.log('visualizing');
+  var dates = _.map(data, 'date');
 
-  // we attach the svg to the html here
-  var svg = d3.select('#content').append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+  var amounts = _.map(data, 'amount');
 
-  // defining path function to draw the line
-  var path = d3.svg.line()
-    .x(function(d) {
-      return x(d.date)
-    })
-    .y(function(d) {
-      return y(d.amount)
-    })
-    .interpolate('basis')
+  y.domain(d3.extent(amounts))
+  x.domain(d3.extent(dates))
 
-  // now we append the path to the svg - note the 2 different options
-  svg.append('path')
-    .attr('class', 'line')
-    .attr('d', path(dataSet))
+  var updateSvg = d3.select("#content").transition();
 
-  // towards the end of the code we add the axes
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+  updateSvg.select(".line")
+    .duration(500)
+    .attr("d", line(data))
+  updateSvg.select(".x.axis")
+    .duration(500)
     .call(xAxis);
-  svg.append("g")
-    .attr("class", "y axis")
+  updateSvg.select(".y.axis")
+    .duration(500)
     .call(yAxis);
-})
+
+}
+
+$('select').on('change', (e, i, v) => {
+  var filerId = $('select option:selected').val();
+  fetchData(filerId)
+    .then(value => {
+      return formatData(value)
+    })
+    .then(value => {
+      return visualize(value)
+    })
+});
+
+function initialize(id) {
+  let url = 'http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/' + id + '/';
+  d3.json(url, (json) => {
+    let dataSet = formatData(json);
+
+    svg.append('path')
+      .attr('class', 'line')
+      .attr('d', line(dataSet));
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+    visualize(dataSet);
+  })
+}
+
+initialize(931);
