7a1bddad74bd70f16c1f319f2d76a52ea6895d1b
April Johnson <ajohnson@extensis.com>
2016-07-25 01:00:10 -0700

    homework week 1

--- lessons/main.js
+++ lessons/main.js
@@ -8,74 +8,118 @@ var margin = {
 var width = 800 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
 
-var filerId = 13920;
+// function to help us sort through the dates
+function sortByDates(a, b) {
+  a = new Date(a);
+  b = new Date(b);
+  return a < b ? -1 : a > b ? 1 : 0;
+}
 
-var url = 'http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/' + filerId + '/';
+// we attach the svg to the html here
+var svg = d3.select('#content').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json(url, function(json) {
-  var data = json;
-  var parseDate = d3.time.format('%Y-%m-%d').parse;
-  var dataSet = data.map(function(item) {
-    return {
-      date: parseDate(item.tran_date),
-      amount: item.amount
-    }
-  })
+function getFilerURL(filerId) {
+  return 'http://54.213.83.132/hackoregon/http/current_candidate_transactions_out/' + filerId + '/';
+}
 
-  // function to help us sort through the dates
-  function sortByDates(a, b) {
-    return a.date - b.date; // dates convert to number so subtraction gives us the sort method
+function getTotalStateURL() {
+  return 'http://54.213.83.132/hackoregon/http/state_sum_by_date/_/';
+}
+
+function changeOption(filerId, firstTime) {
+  var url;
+  if (filerId === "state") {
+    url = getTotalStateURL();
+  } else {
+    url = getFilerURL(filerId)
   }
 
-  var dates = _.map(dataSet, 'date');
-  var amounts = _.map(dataSet, 'amount');
-
-  // defining the x and y values
-  var x = d3.time.scale() // determined through d3.time.scale function
-    .domain(d3.extent(dates)) // using the extent method on dates array
-    .range([0, width]);
-  var y = d3.scale.linear()
-    .domain(d3.extent(amounts))
-    .range([height, 0]);
-
-  // sort data here
-  dataSet.sort(sortByDates);
-
-  // defining axes
-  var xAxis = d3.svg.axis().scale(x)
-    .orient('bottom').ticks(6);
-  var yAxis = d3.svg.axis().scale(y)
-    .orient('left').ticks(10);
-
-  // we attach the svg to the html here
-  var svg = d3.select('#content').append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
-
-  // defining path function to draw the line
-  var path = d3.svg.line()
-    .x(function(d) {
-      return x(d.date)
+  d3.json(url, function(json) {
+    var data = json;
+    var parseDate = d3.time.format('%Y-%m-%d').parse;
+    var dataSet = data.map(function (item) {
+      return {
+        date: d3.time.month(parseDate(item.tran_date)),
+        amount: (filerId === 'state') ? item.total_in : item.amount
+      }
     })
-    .y(function(d) {
-      return y(d.amount)
-    })
-    .interpolate('basis')
 
-  // now we append the path to the svg - note the 2 different options
-  svg.append('path')
-    .attr('class', 'line')
-    .attr('d', path(dataSet))
+    var nestedDataSet = d3.nest()
+      .key(function (d) {
+        return d.date;
+      })
+      .sortKeys(sortByDates)
+      .rollup(function (v) {
+        return {
+          amount: d3.sum(v, function (d) {
+            return d.amount;
+          })
+        };
+      })
+      .entries(dataSet);
 
-  // towards the end of the code we add the axes
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-  svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-})
+    var dates = _.map(nestedDataSet, function (value) {
+      return new Date(value.key)
+    });
+    var amounts = _.map(nestedDataSet, function (value) {
+      return value.values.amount
+    });
+
+    // defining the x and y values
+    var x = d3.time.scale() // determined through d3.time.scale function
+      .domain(d3.extent(dates)) // using the extent method on dates array
+      .range([0, width]);
+    var y = d3.scale.linear()
+      .domain(d3.extent(amounts))
+      .range([height, 0]);
+
+    // defining axes
+    var xAxis = d3.svg.axis().scale(x)
+      .orient('bottom').ticks(6);
+    var yAxis = d3.svg.axis().scale(y)
+      .orient('left').ticks(10);
+
+    // defining path function to draw the line
+    var path = d3.svg.line()
+      .x(function (d) {
+        return x(new Date(d.key))
+      })
+      .y(function (d) {
+        return y(d.values.amount)
+      })
+      .interpolate('basis')
+
+    if (firstTime) {
+      // now we append the path to the svg - note the 2 different options
+      svg.append('path')
+        .attr('class', 'line')
+        .attr('d', path(nestedDataSet))
+
+      // towards the end of the code we add the axes
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+    } else {
+      // Make the changes
+      svg.select(".line")   // change the line
+        .transition() //
+        .duration(500)
+        .attr("d", path(nestedDataSet));
+      svg.select(".x.axis") // change the x axis
+        .call(xAxis);
+      svg.select(".y.axis") // change the y axis
+        .call(yAxis);
+    }
+  })
+}
+
+changeOption('state', true)
